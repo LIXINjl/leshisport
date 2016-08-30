415291956e8e32fcef5783bc8187b892c4ca7229
lixin <598767832@qq.com>
2016-08-30 10:35:40 +0800

    11

--- js/AutoBanner.js
+++ js/AutoBanner.js
@@ -1,125 +1,128 @@
-new Lei("content1", 3000, 0);
-new Lei("content2", 2500, 1);
-new Lei("content3", 2800, 2);
-new Lei("content4", 2200, 3);
-new Lei("content5", 2600, 4);
-function Lei(id, times, m) {
-    /*整屏-大背景焦点图 begin*/
-    //大图选择
-    var oContent1 = document.getElementById(id);
-    var oFocus = oContent1.getElementsByTagName("div")[0];
-    var oAs = oFocus.getElementsByTagName('a');
-    //小图选择
-    var oPic = oContent1.getElementsByTagName("div")[1];
-    var oUl = oPic.getElementsByTagName('ul')[0];
-    var oLis = oUl.getElementsByTagName('li');
-    var oImgs = oLis[0].getElementsByTagName('img');
-//自动播放的轮播图
-    var step = 0;
-    var autoTimer = null;
-    zhufengAnimate(oAs[0], {'opacity': 1}, 800);
-    utils.css(oAs[0], "zIndex", 1);
-    autoTimer = setInterval(autoMove, times);
+(function(){
+    function AutoBanner(curEleId,url,interval){
+        this.banner=document.getElementById(curEleId);
+        this.banner_box=utils.firstChild(this.banner);
+        this.imgList=this.banner_box.getElementsByTagName('a');
+        this.small_box=utils.children(this.banner)[1];
+        this.infoTitle=utils.lastChild(this.banner);
+        this.info_title=this.infoTitle.getElementsByTagName('div');
+        this.small_box_img=utils.firstChild(this.small_box);
+        this.small_imgList=this.small_box_img.getElementsByTagName('img');
+        this.oLis=this.small_box_img.getElementsByTagName('li');
 
-    function autoMove() {
-        if (step >= oAs.length - 1) {
-            step = -1;
-        }
-        step++;
-        setBanner();
+        this.jsonData=null;
+        this.url=url;
+        this.interval=interval||3000;
+        this.step=0;
+        this.timer=0;
+        return this.init();
     }
-
-    function setBanner() {
-        for (var i = 0; i < oAs.length; i++) {
-            var curA = oAs[i];
-            oDiv[i].style.display = "none";
-            if (i === step) {
-                utils.css(curA, 'zIndex', 1);
-                oDiv[i].style.display = "block";
-                zhufengAnimate(curA, {'opacity': 1}, 1500, function () {
-                    var siblings = utils.siblings(this);
-                    for (var i = 0; i < siblings.length; i++) {
-                        utils.css(siblings[i], 'opacity', 0);
-                        //zhufengAnimate(siblings[i],{'opacity':0},800);
-                    }
-                });
-                continue;
-            }
-            utils.css(curA, 'zIndex', 0);
-        }
-        bannerTip();
-        //handleChange();
-    }
-
-//焦点图自动轮播
-    function bannerTip() {
-        for (var i = 0; i < oLis.length; i++) {
-            var curLi = oLis[i];
-            i === step ? utils.addClass(curLi, 'bg') : utils.removeClass(curLi, 'bg');
-        }
-    }
-
-//点击焦点，手动切换    中间的标签栏也跟随轮播
-    /*handleChange();
-     function handleChange(){
-     for(var i=0;i<oLis.length;i++){
-     var curLi=oLis[i];
-     curLi.index=i;
-     curLi.onmouseover=function(){
-     step=this.index;
-     setBanner();
-     }
-     }
-     }
-     var oFloat=document.getElementsByClassName('floatMin')[0];
-     var oDiv=oFloat.getElementsByTagName('div');
-     for(var i=0;i<oLis.length;i++){
-     oLis[i].index=i;
-     oDiv[0].style.display="block";
-     oLis[i].onmouseover=function(){
-     clearInterval(autoTimer);
-     for(var j=0;j<oDiv.length;j++){
-     oDiv[j].style.display="none";
-     }
-     oDiv[this.index].style.display="block";
-     }
-     }*/
-    var oFloat = document.getElementsByClassName('floatMin')[m];
-    var oDiv = oFloat.getElementsByTagName('div');
-    handleChange();
-    function handleChange() {
-        for (var i = 0; i < oLis.length; i++) {
-            var curLi = oLis[i];
-            curLi.index = i;
-            oDiv[0].style.display = "block";
-            curLi.onmouseover = function () {
-                clearInterval(autoTimer);
-                step = this.index;
-                setBanner();
-                for (var j = 0; j < oDiv.length; j++) {
-                    oDiv[j].style.display = "none";
+    AutoBanner.prototype={
+        constructor:AutoBanner,
+        getData:function(){
+            var _this=this;
+            var xhr = new XMLHttpRequest;
+            xhr.open("get", this.url+ "?_="+ Math.random(), false);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && /^2\d{2}$/.test(xhr.status)) {
+                    _this.jsonData = utils.formatJSON(xhr.responseText);
                 }
-                oDiv[this.index].style.display = "block";
             };
-            curLi.onmouseout = function () {
-                autoTimer = setInterval(autoMove, times);
+            xhr.send(null);
+        },
+        bindData:function(){
+            var str1="",str2="",str3="";
+            for(var i= 0,len=this.jsonData.length;i<len;i++){
+                var curData=this.jsonData[i];
+                var str='url("'+curData["img1"]+'") 50% 0 / cover no-repeat fixed';
+                str1+="<a href='javascript:;' trueImg='"+str+"'></a>";
+                i==0?str2+="<li style='bottom: 0'><img trueImg='"+this.jsonData[0]["img2"]+"' src='' alt=''></li>":str2+="<li><img trueImg='"+curData["img2"]+"' src='' alt=''></li>";
+                i==0?str3+="<div class='info_title' con_id='"+i+"' style='display: block'><h3><a>"+curData["h3"]+"</a></h3><p>"+curData["p"]+"</p></div>":str3+="<div class='info_title' con_id='"+i+"'><h3><a>"+curData["h3"]+"</a></h3><p>"+curData["p"]+"</p></div>"
             }
+            this.banner_box.innerHTML=str1;
+            this.small_box_img.innerHTML=str2;
+            this.infoTitle.innerHTML+=str3;
+        },
+        lazyImg:function(){
+            var _this=this;
+            for(var i= 0,len=_this.imgList.length;i<len;i++){
+                ~function(i){
+                    var curImg=_this.imgList[i];
+                    var curSmallImg=_this.small_imgList[i];
+                    var oImg=new Image;
+                    oImg.src=curSmallImg.getAttribute('trueImg');
+                    oImg.onload=function(){
+                        curSmallImg.src=this.src;
+                        curSmallImg.style.display='block';
+                        curImg.style.background=curImg.getAttribute('trueImg');
+                        if(i==0){
+                            utils.css(curImg,'zIndex',1);
+                            zhufengAnimate(curImg,{opacity:1},300)
+                        }
+                        oImg=null;
+                    }
+                }(i);
+            }
+        },
+        autoMove:function(){
+            if(this.step==(this.jsonData.length-1)){
+                this.step=-1;
+            }
+            this.step++;
+            this.setBanner();
+        },
+        setBanner: function(){
+            for(var i= 0,len=this.imgList.length;i<len;i++){
+                var curImg=this.imgList[i];
+                zhufengAnimate(curImg,{opacity:0},700);
+                if(i==this.step){
+                    utils.css(curImg,'zIndex',1);
+                    zhufengAnimate(curImg,{opacity:1},700);
+                    continue;
+                }
+                utils.css(curImg,'zIndex',0);
+            }
+            for(i= 0;i<len;i++){
+                var curLi=this.oLis[i];
+                utils.css(curLi, 'bottom', -15);
+                i==this.step?utils.css(curLi, 'bottom', 0):null;
+            }
+            for(i= 0;i<len;i++){
+                var curInfo=this.info_title[i];
+                var con_id=curInfo.getAttribute("con_id");
+                curInfo.style.display=con_id==this.step?"block":"none";
+            }
+        },
+        tipEvent: function () {
+            var _this = this;
+            for (var i = 0, len = this.oLis.length; i < len; i++) {
+                var curLi = this.oLis[i];
+                curLi.index = i;
+                curLi.onmouseover = function () {
+                    window.clearTimeout(_this.timer);
+                    _this.step = this.index;
+                    _this.setBanner();
+                };
+                curLi.onmouseout = function () {
+                    _this.timer=window.setInterval(function(){
+                        _this.autoMove();
+                    },_this.interval);
+                }
+            }
+        },
+        init:function(){
+            var _this=this;
+            this.getData();
+            this.bindData();
+            window.setTimeout(function(){
+                _this.lazyImg();
+            },500);
+            this.timer=window.setInterval(function(){
+                _this.autoMove();
+            },this.interval);
+            this.tipEvent();
+            return this;
         }
-    }
-}
-
-//点击效果轮播
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    };
+    window.AutoBanner=AutoBanner;
+})();
--- js/utils.js
+++ js/utils.js
@@ -1,208 +1,75 @@
-/**
- * Created by xiao lei on 2016/5/15.
- */
 var utils = (function () {
-    var flag = 'getComputedStyle' in window;
-    //listToArray:类数组转数组
-    function listToArray(arg) {
+    var flag = "getComputedStyle" in window;
+
+    //->listToArray:把类数组集合转换为数组
+    function listToArray(likeAry) {
         if (flag) {
-            return Array.prototype.slice.call(arg);
-        } else {
-            var ary = [];
-            for (var i = 0; i < arg.length; i++) {
-                ary.push(arg[i]);
-            }
-            return ary;
+            return Array.prototype.slice.call(likeAry, 0);
         }
+        var ary = [];
+        for (var i = 0; i < likeAry.length; i++) {
+            ary[ary.length] = likeAry[i];
+        }
+        return ary;
     }
 
-    //jsonParse:JSON格式的字符串转JSON格式数据
-    function jsonParse(str) {
-        return flag ? JSON.parse(str) : eval('(' + str + ')');
+    //->formatJSON:把JSON格式字符串转换为JSON格式对象
+    function formatJSON(jsonStr) {
+        return "JSON" in window ? JSON.parse(jsonStr) : eval("(" + jsonStr + ")");
     }
 
-    //offset:当前元素距离body的偏移量
+    //->offset:获取页面中任意元素距离BODY的偏移
     function offset(curEle) {
-        var l = 0;
-        var t = 0;
-        var par = curEle.offsetParent;
-        l += curEle.offsetLeft;
-        t += curEle.offsetTop;
+        var disLeft = curEle.offsetLeft, disTop = curEle.offsetTop, par = curEle.offsetParent;
         while (par) {
-            //IE8 offsetLeft/top已经包含了边框，但是其他浏览器不包含边框；
-            if (navigator.userAgent.indexOf('MSIE 8.0') === -1) {
-                l += par.clientLeft;
-                t += par.clientTop;
+            if (navigator.userAgent.indexOf("MSIE 8") === -1) {
+                disLeft += par.clientLeft;
+                disTop += par.clientTop;
             }
-            l += par.offsetLeft;
-            t += par.offsetTop;
+            disLeft += par.offsetLeft;
+            disTop += par.offsetTop;
             par = par.offsetParent;
-
         }
-        return {left: l, top: t}
+        return {left: disLeft, top: disTop};
     }
 
-    //win:获取和设置浏览器盒子模型；
+    //->win:操作浏览器的盒子模型信息
     function win(attr, value) {
-        if (typeof value === 'undefined') {
+        if (typeof value === "undefined") {
             return document.documentElement[attr] || document.body[attr];
         }
-        document.documentElement[attr] = document.body[attr] = value;
+        document.documentElement[attr] = value;
+        document.body[attr] = value;
     }
 
-    //getByClass:在一定范围内通过className获取元素
-    function getByClass(curEle, strClass) {
-        if (flag) {//高级
-            return this.listToArray(curEle.getElementsByClassName(strClass));
-        }
-        var ary = [];
-        var aryClass = strClass.replace(/(^\s+)|(\s+$)/g, '').split(/\s+/g);
-        var nodeList = curEle.getElementsByTagName('*');//拿到当前元素下所有元素
-        for (var i = 0; i < nodeList.length; i++) {//循环：目的是为了匹配每个元素的className是否符合要求，匹配要求：是这个元素上的className符合aryclass中的每一个className字符串
-            var curNode = nodeList[i];
-            var bOk = true;//假设法：假设都符合
-            for (var k = 0; k < aryClass.length; k++) {
-                var curClass = aryClass[k];
-                //var reg=new RegExp('(\\b)'+curClass+'(\\b)');
-                var reg = new RegExp('(^| +)' + curClass + '( +|$)');
-                if (!reg.test(curNode.className)) {
-                    bOk = false;
-                }
-            }
-            if (bOk) {
-                ary.push(curNode)
-            }
-        }
-        return ary;
-    }
-
-    //hasClass:判断当前元素上是否有这个strClass（class名）
-    function hasClass(curEle, strClass) {
-        var reg = new RegExp('(\\b)' + strClass + '(\\b)');
-        return reg.test(curEle.className)
-    }
-
-    //addClass:添加一堆class名
-    function addClass(curEle, strClass) {
-        var aryClass = strClass.replace(/(^\s+)|(\s+$)/g, '').split(/\s+/g);
-        for (var i = 0; i < aryClass.length; i++) {
-            var curClass = aryClass[i];
-            if (!this.hasClass(curEle, curClass)) {
-                curEle.className += ' ' + curClass;
-            }
-        }
-
-    }
-
-    //removeClass:移除掉当前元素上的class名
-    function removeClass(curEle, strClass) {
-        var aryClass = strClass.replace(/(^\s+)|(\s+$)/g, '').split(/\s+/g);
-        for (var i = 0; i < aryClass.length; i++) {
-            var curClass = aryClass[i];
-            if (this.hasClass(curEle, curClass)) {
-                var reg = new RegExp('(^| +)' + curClass + '( +|$)')
-                curEle.className = curEle.className.replace(reg, ' ')
-            }
-        }
-
-    }
-
-    //getCss:获取非行间样式
-    function getCss(curEle, attr) {
-        var val = null;
-        var reg = null;
-        if (flag) {//高级浏览器
-            val = getComputedStyle(curEle, null)[attr];
-        } else {//低级浏览器
-            if (attr == 'opacity') {// alpha(opacity=10)
-                val = curEle.currentStyle['filter'];
-                reg = /^alpha\(opacity[=:](\d+(?:\.\d+))?\)$/i;
-                return reg.test(val) ? reg.exec(val)[1] / 100 : 1;
-            } else {
-                val = curEle.currentStyle[attr];
-            }
-
-        }
-        reg = /^([+-]?\d+(\.\d+)?)(px|pt|em|rem)?$/i;//-200px +200px 22.33px px pt em rem
-        // reg=/^((\+|-)?\d+(\.\d+)?)(px|pt|em|rem)?$/i;
-        return reg.test(val) ? parseFloat(val) : val;
-    }
-
-    //setCss:设置行间样式
-    function setCss(curEle, attr, value) {
-        //float
-        if (attr == 'float') {
-            curEle.style.cssFloat = value;//火狐
-            curEle.style.styleFloat = value;//ie
-            return;
-        }
-        //透明度的处理
-        if (attr === 'opacity') {
-            curEle.style.opacity = value;
-            curEle.style.filter = 'alpha(opacity=' + value * 100 + ')';
-            return;
-        }
-        //加单位的处理；
-        var reg = /(width|height|top|right|bottom|left|((margin|padding)(top|right|bottom|left)?))/
-        if (reg.test(attr)) {
-            value += 'px';
-        }
-        curEle.style[attr] = value;
-    }
-
-    //setGroupCss:设置一组样式
-    function setGroupCss(curEle, options) {
-        if (options.toString() !== '[object Object]') {
-            return;
-        }
-        for (var attr in options) {
-            this.setCss(curEle, attr, options[attr])
-        }
-    }
-
-    //css:获取和设置样式
-    function css(curEle) {
-        var argTwo = arguments[1];
-        if (typeof argTwo === 'string') {
-            if (typeof arguments[2] !== 'undefined') {// 单个设置
-                this.setCss(curEle, argTwo, arguments[2]);
-                return;
-            } else {//获取
-                return this.getCss(curEle, argTwo)
-            }
-        }
-        argTwo = argTwo || 0;
-        if (argTwo.toString() === '[object Object]') {//设置一组样式
-            this.setGroupCss(curEle, argTwo)
-        }
-    }
-
-    //children:获取当前元素的所有子节点
+    //->children:获取所有的元素子节点
     function children(curEle, tagName) {
         var ary = [];
-        if (flag) {
-            ary = this.listToArray(curEle.children);
-        } else {
-            var chs = curEle.childNodes;
-            for (var i = 0; i < chs.length; i++) {
-                var curNode = chs[i];
-                if (curNode.nodeType == 1) {
-                    ary.push(curNode)
-                }
+        if (!flag) {
+            var nodeList = curEle.childNodes;
+            for (var i = 0, len = nodeList.length; i < len; i++) {
+                var curNode = nodeList[i];
+                curNode.nodeType === 1 ? ary[ary.length] = curNode : null;
             }
+            nodeList = null;
+        } else {
+            ary = this.listToArray(curEle.children);
         }
-        if (typeof tagName == 'string') {
-            for (var i = 0; i < ary.length; i++) {
-                if (ary[i].nodeName.toLowerCase() !== tagName.toLowerCase()) {
-                    ary.splice(i, 1);
-                    i--;
+        if (typeof tagName === "string") {
+            for (var k = 0; k < ary.length; k++) {
+                var curEleNode = ary[k];
+                if (curEleNode.nodeName.toLowerCase() !== tagName.toLowerCase()) {
+                    ary.splice(k, 1);
+                    k--;
                 }
             }
         }
         return ary;
     }
 
-    //prev:获取上一个哥哥元素
+
+    //->prev:获取上一个哥哥元素节点
+    //->首先获取当前元素的上一个哥哥节点,判断是否为元素节点,不是的话基于当前的继续找上面的哥哥节点...一直到找到哥哥元素节点为止,如果没有哥哥元素节点,返回null即可
     function prev(curEle) {
         if (flag) {
             return curEle.previousElementSibling;
@@ -214,18 +81,7 @@ var utils = (function () {
         return pre;
     }
 
-    //prevAll:获取所有的哥哥元素节点
-    function prevAll(curEle) {
-        var pre = this.prev(curEle);
-        var ary = [];
-        while (pre) {
-            ary.unshift(pre);
-            pre = this.prev(pre);
-        }
-        return ary;
-    }
-
-    //next:下一个弟弟元素节点
+    //->next:获取下一个弟弟元素节点
     function next(curEle) {
         if (flag) {
             return curEle.nextElementSibling;
@@ -237,10 +93,21 @@ var utils = (function () {
         return nex;
     }
 
-    //nextAll:所有的弟弟元素节点
-    function nextAll(curEle) {
-        var nex = this.next(curEle);
+    //->prevAll:获取所有的哥哥元素节点
+    function prevAll(curEle) {
         var ary = [];
+        var pre = this.prev(curEle);
+        while (pre) {
+            ary.unshift(pre);
+            pre = this.prev(pre);
+        }
+        return ary;
+    }
+
+    //->nextAll:获取所有的弟弟元素节点
+    function nextAll(curEle) {
+        var ary = [];
+        var nex = this.next(curEle);
         while (nex) {
             ary.push(nex);
             nex = this.next(nex);
@@ -248,105 +115,214 @@ var utils = (function () {
         return ary;
     }
 
-    //sibling:相邻元素节点
+    //->sibling:获取相邻的两个元素节点
     function sibling(curEle) {
-        var pre = this.prev(curEle)
+        var pre = this.prev(curEle);
         var nex = this.next(curEle);
         var ary = [];
-        if (pre) ary.push(pre);
-        if (nex) ary.push(nex);
+        pre ? ary.push(pre) : null;
+        nex ? ary.push(nex) : null;
         return ary;
     }
 
-    //siblings:兄弟元素节点
+    //->siblings:获取所有的兄弟元素节点
     function siblings(curEle) {
-        return this.prevAll(curEle).concat(this.nextAll(curEle))
+        return this.prevAll(curEle).concat(this.nextAll(curEle));
     }
 
-    //firstChild:第一个子元素
-    function firstChild(curEle) {
-        var chs = this.children(curEle);
-        return chs.length ? chs[0] : null;
-    }
-
-    //lastChild:最后一个子元素
-    function lastChild(curEle) {
-        var chs = this.children(curEle);
-        return chs.length ? chs[chs.length - 1] : null;
-    }
-
-    //index:求当前元素的索引；
+    //->index:获取当前元素的索引
     function index(curEle) {
         return this.prevAll(curEle).length;
     }
 
-    //appendChild:把元素插入到容器的末尾
-    function appendChild(context, curEle) {
-        context.appendChild(curEle);
+    //->firstChild:获取第一个元素子节点
+    function firstChild(curEle) {
+        var chs = this.children(curEle);
+        return chs.length > 0 ? chs[0] : null;
     }
 
-    //prepend:把元素插入到容器的最开头
-    function prepend(context, curEle) {
-        var fir = this.firstChild(context);
+    //->lastChild:获取最后一个元素子节点
+    function lastChild(curEle) {
+        var chs = this.children(curEle);
+        return chs.length > 0 ? chs[chs.length - 1] : null;
+    }
+
+    //->append:向指定容器的末尾追加元素
+    function append(newEle, container) {
+        container.appendChild(newEle);
+    }
+
+    //->prepend:向指定容器的开头追加元素
+    //->把新的元素添加到容器中第一个子元素节点的前面,如果一个元素子节点都没有,就放在末尾即可
+    function prepend(newEle, container) {
+        var fir = this.firstChild(container);
         if (fir) {
-            context.insertBefore(curEle, fir);
-        } else {
-            context.appendChild(curEle);
+            container.insertBefore(newEle, fir);
+            return;
         }
+        container.appendChild(newEle);
     }
 
-    //insertBefore:把某个元素插入到指定元素的前面
-    function insertBefore(curEle, oldEle) {
-        oldEle.parentNode.insertBefore(curEle, oldEle);
+    //->insertBefore:把新元素(newEle)追加到指定元素(oldEle)的前面
+    function insertBefore(newEle, oldEle) {
+        oldEle.parentNode.insertBefore(newEle, oldEle);
     }
 
-    //insertAfter:把某个元素插入到指定元素的后面
-    function insertAfter(curEle, oldEle) {
+    //->insertAfter:把新元素(newEle)追加到指定元素(oldEle)的后面
+    //->相当于追加到oldEle弟弟元素的前面,如果弟弟不存在,也就是当前元素已经是最后一个了,我们把新的元素放在最末尾即可
+    function insertAfter(newEle, oldEle) {
         var nex = this.next(oldEle);
         if (nex) {
-            oldEle.parentNode.insertBefore(curEle, nex);
-        } else {
-            oldEle.parentNode.appendChild(curEle);
+            oldEle.parentNode.insertBefore(newEle, nex);
+            return;
         }
-
+        oldEle.parentNode.appendChild(newEle);
     }
 
+
+    //->hasClass:验证当前元素中是否包含className这个样式类名
+    function hasClass(curEle, className) {
+        var reg = new RegExp("(^| +)" + className + "( +|$)");
+        return reg.test(curEle.className);
+    }
+
+    //->addClass:给元素增加样式类名
+    function addClass(curEle, className) {
+        var ary = className.replace(/(^ +| +$)/g, "").split(/ +/g);
+        for (var i = 0, len = ary.length; i < len; i++) {
+            var curName = ary[i];
+            if (!this.hasClass(curEle, curName)) {
+                curEle.className += " " + curName;
+            }
+        }
+    }
+
+    //->removeClass:给元素移除样式类名
+    function removeClass(curEle, className) {
+        var ary = className.replace(/(^ +| +$)/g, "").split(/ +/g);
+        for (var i = 0, len = ary.length; i < len; i++) {
+            var curName = ary[i];
+            if (this.hasClass(curEle, curName)) {
+                var reg = new RegExp("(^| +)" + curName + "( +|$)", "g");
+                curEle.className = curEle.className.replace(reg, " ");
+            }
+        }
+    }
+
+    //->getElementsByClass:通过元素的样式类名获取一组元素集合
+    function getElementsByClass(strClass, context) {
+        context = context || document;
+        if (flag) {
+            return this.listToArray(context.getElementsByClassName(strClass));
+        }
+        //->IE6~8
+        var ary = [], strClassAry = strClass.replace(/(^ +| +$)/g, "").split(/ +/g);
+        var nodeList = context.getElementsByTagName("*");
+        for (var i = 0, len = nodeList.length; i < len; i++) {
+            var curNode = nodeList[i];
+            var isOk = true;
+            for (var k = 0; k < strClassAry.length; k++) {
+                var reg = new RegExp("(^| +)" + strClassAry[k] + "( +|$)");
+                if (!reg.test(curNode.className)) {
+                    isOk = false;
+                    break;
+                }
+            }
+            if (isOk) {
+                ary[ary.length] = curNode;
+            }
+        }
+        return ary;
+    }
+
+    //->getCss:获取元素的样式值
+    function getCss(attr) {
+        var val = null, reg = null;
+        if (flag) {
+            val = window.getComputedStyle(this, null)[attr];
+        } else {
+            if (attr === "opacity") {
+                val = this.currentStyle["filter"];
+                reg = /^alpha\(opacity=(\d+(?:\.\d+)?)\)$/;
+                val = reg.test(val) ? reg.exec(val)[1] / 100 : 1;
+            } else {
+                val = this.currentStyle[attr];
+            }
+        }
+        reg = /^(-?\d+(\.\d+)?)(px|pt|em|rem)?$/;
+        return reg.test(val) ? parseFloat(val) : val;
+    }
+
+    //->setCss:给当前元素的某一个样式属性设置值(增加在行内样式上的)
+    function setCss(attr, value) {
+        if (attr === "float") {
+            this["style"]["cssFloat"] = value;
+            this["style"]["styleFloat"] = value;
+            return;
+        }
+        if (attr === "opacity") {
+            this["style"]["opacity"] = value;
+            this["style"]["filter"] = "alpha(opacity=" + value * 100 + ")";
+            return;
+        }
+        var reg = /^(width|height|top|bottom|left|right|fontSize|((margin|padding)(Top|Bottom|Left|Right)?))$/;
+        if (reg.test(attr)) {
+            if (!isNaN(value)) {
+                value += "px";
+            }
+        }
+        this["style"][attr] = value;
+        console.log(attr, value);
+    }
+
+    //->setGroupCss:给当前元素批量的设置样式属性值
+    function setGroupCss(options) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                setCss.call(this, key, options[key]);
+            }
+        }
+    }
+
+    //->css:此方法实现了获取、单独设置、批量设置元素的样式值
+    function css(curEle) {
+        var argTwo = arguments[1], ary = Array.prototype.slice.call(arguments, 1);
+        if (typeof argTwo === "string") {
+            if (typeof arguments[2] === "undefined") {
+                return getCss.apply(curEle, ary);
+            }
+            setCss.apply(curEle, ary);
+        }
+        argTwo = argTwo || 0;
+        if (argTwo.toString() === "[object Object]") {
+            setGroupCss.apply(curEle, ary);
+        }
+    }
+
+    //->把外界需要使用的方法暴露给utils
     return {
-        listToArray: listToArray,
-        jsonParse: jsonParse,
-        offset: offset,
         win: win,
-        getByClass: getByClass,
-        hasClass: hasClass,
-        addClass: addClass,
-        removeClass: removeClass,
-        getCss: getCss,
-        setCss: setCss,
-        setGroupCss: setGroupCss,
-        css: css,
+        offset: offset,
+        listToArray: listToArray,
+        formatJSON: formatJSON,
         children: children,
         prev: prev,
-        prevAll: prevAll,
         next: next,
+        prevAll: prevAll,
         nextAll: nextAll,
         sibling: sibling,
         siblings: siblings,
+        index: index,
         firstChild: firstChild,
         lastChild: lastChild,
-        index: index,
-        appendChild: appendChild,
+        append: append,
         prepend: prepend,
         insertBefore: insertBefore,
-        insertAfter: insertAfter
-
+        insertAfter: insertAfter,
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        getElementsByClass: getElementsByClass,
+        css: css
     }
 })();
-
-
-
-
-
-
-
-
-
